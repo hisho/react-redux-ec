23bd93427708c4d0ad0fc525c0c56009409b6031
hisho <hisho.web@gmail.com>
2021-05-05 23:11:08 +0900

    ✨ route signin signup page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Route, Switch} from "react-router";
-import {Home, Login} from "@src/pages";
+import {Home, SignIn, SignUp} from "@src/pages";
 
 export const Router: React.VFC = () => {
   return (
     <Switch>
-      <Route exact path="/login" component={Login}/>
+      <Route exact path="/signin" component={SignIn}/>
+      <Route exact path="/signup" component={SignUp}/>
       <Route exact path="(/)?" component={Home}/>
     </Switch>
   )
