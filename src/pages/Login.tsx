253020db3c10f9d4ed15ce7870887af63654399c
hisho <hisho.web@gmail.com>
2021-05-05 17:11:03 +0900

    ✨ update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {push} from "connected-react-router";
+import {signInAction} from "@src/reducks/users/actions";
 
 export const Login:React.VFC = () => {
   const dispatch = useDispatch();
@@ -11,7 +12,10 @@ export const Login:React.VFC = () => {
   return (
     <>
       <h2>ログイン</h2>
-      <button type="button" onClick={() => dispatch(push('/'))}>
+      <button type="button" onClick={() => {
+        dispatch(signInAction({uid: "00001", username: "test"}))
+        dispatch(push('/'))
+      }}>
         ログインする
       </button>
     </>
