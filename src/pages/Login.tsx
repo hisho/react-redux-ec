4dd47caebc073ef6bea2da33d564d4ef95b5ec63
hisho <hisho.web@gmail.com>
2021-05-05 17:53:50 +0900

    ✨ add Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {push} from "connected-react-router";
-import {signInAction} from "@src/reducks/users/actions";
+import {useDispatch} from "react-redux";
+import {signIn} from "@src/reducks/users/operations";
 
 export const Login:React.VFC = () => {
   const dispatch = useDispatch();
-  const selector = useSelector(state => state);
-
-  console.log(selector);
 
   return (
     <>
       <h2>ログイン</h2>
-      <button type="button" onClick={() => {
-        dispatch(signInAction({uid: "00001", username: "test"}))
-        dispatch(push('/'))
-      }}>
+      <button type="button" onClick={() => dispatch(signIn())}>
         ログインする
       </button>
     </>
