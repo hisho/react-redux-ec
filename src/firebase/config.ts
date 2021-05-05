ddcdb9b064d8374d22095f877b9933aa52e79544
hisho <hisho.web@gmail.com>
2021-05-05 23:11:21 +0900

    ✨ add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC7Zl9AtMVOgM_wpS5ZJH0iMp71N93vJoA",
+  authDomain: "ec-app-88c99.firebaseapp.com",
+  projectId: "ec-app-88c99",
+  storageBucket: "ec-app-88c99.appspot.com",
+  messagingSenderId: "80991888911",
+  appId: "1:80991888911:web:fdd20fd5edbdf0f429257a",
+  measurementId: "G-FZ5L0LZ04L"
+} as const;
