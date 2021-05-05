7455db3636b758e465d1e0a62e188580edae4bd9
hisho <hisho.web@gmail.com>
2021-05-05 23:12:06 +0900

    ✨ add signin sigup operations

--- src/reducks/users/operations.ts
+++ src/reducks/users/operations.ts
@@ -1,35 +1,94 @@
 import React from "react";
-import {signInAction} from "@src/reducks/users/actions";
 import {push} from "connected-react-router";
+import {auth, db, FirebaseTimestamp} from "@src/firebase";
+import {signInAction} from "@src/reducks/users/actions";
 import {RootStateType} from "@src/reducks/type";
 
 //サインインする時に実行する関数
-export const signIn = () => {
-  return async (dispatch: React.Dispatch<unknown>, getState: () => RootStateType) => {
-    //現在のユーザーの値を取得
-    const {users} = getState();
+export const signIn = (email: string, password: string) => {
+  return async (dispatch: React.Dispatch<unknown>) => {
+    //バリデーション
+    //emailとpasswordが空の場合alertを出す
+    //TODO バリデーションをちゃんとする
+    if (email === "" || password === '') {
+      alert("必須項目が未入力です");
+      return false;
+    }
 
-    //現在ユーザーがサインインしているかどうか
-    const isSignedIn = users.isSignedIn;
+    //メアドとパスワードで認証する
+    auth.signInWithEmailAndPassword(email, password)
+      .then(result => {
+        const user = result.user;
 
-    //サインインしている場合は早期リターン
-    if (isSignedIn) return;
+        if (user) {
+          const uid = user.uid;
 
-    //ダミー
-    const url = "https://api.github.com/users/hisho";
-    const response: {
-      login: string
-    } = await fetch(url).then(res => res.json()).catch(() => null);
-    const username = response.login;
+          //firebaseのusersからuidを検索してgetする
+          db.collection('users').doc(uid).get()
+            .then((snapshot) => {
+              //snapshotは返った来たユーザーのdata
+              //TODO asを無くす
+              const data = snapshot.data() as Omit<RootStateType["users"], 'isSignedIn'>;
 
-    //uidとusernameを変更してsignInActionを実行
-    //signInAction中でisSignedInをtrueに設定しているためisSignedInは不要
-    dispatch(signInAction({
-      uid: "00001",
-      username
-    }));
+              //ユーザーの認証情報をセットする
+              dispatch(signInAction({
+                role: data.role,
+                uid: data.uid,
+                username: data.username
+              }))
 
-    //サインイン後にトップページへリダイレクト
-    dispatch(push('/'));
+              //Homeに遷移させる
+              dispatch(push('/'))
+            })
+        }
+      })
+  }
+}
+
+export const signUp = (username: string, email: string, password: string, confirmPassword: string) => {
+  return async (dispatch: React.Dispatch<unknown>) => {
+    //バリデーション
+    //usernameとemailとpasswordとconfirmPasswordが空の場合alertを出す
+    //TODO バリデーションをちゃんとする
+    if (username === "" || email === "" || password === '' || confirmPassword === "") {
+      alert("必須項目が未入力です");
+      return false;
+    }
+
+    //passwordとconfirmPasswordが一致しない場合alertを出す
+    //TODO バリデーションをちゃんとする
+    if (password !== confirmPassword) {
+      alert("パスワードが一致しません。");
+      return false;
+    }
+
+    //emailとpasswordでユーザーを作成する
+    return auth.createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        const user = result.user;
+
+        //アカウント作成が成功していたら処理を続ける
+        if (user) {
+          const uid = user.uid;
+          //現在の時間を設定する
+          const timestamp = FirebaseTimestamp.now();
+
+          //ユーザーのデータの雛形に当てはめる
+          const userInitialData = {
+            created_at: timestamp,
+            email,
+            role: "customer",
+            uid,
+            updated_at: timestamp,
+            username
+          }
+
+          //firebaseのusersのuidが一致すれば保存しhomeに遷移させる
+          db.collection('users').doc(uid).set(userInitialData)
+            .then(() => {
+              dispatch(push('/'));
+            })
+        }
+      })
   }
 }
