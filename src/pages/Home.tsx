ab0df48e2ae2165655b91050ed25425ccd360f88
hisho <hisho.web@gmail.com>
2021-05-05 17:10:52 +0900

    ✨ update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import {getUserId} from "@src/reducks/users/selector";
+import {useSelector} from "react-redux";
 
-export const Home:React.VFC = () => {
+export const Home: React.VFC = () => {
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector);
+
   return (
     <>
       this is home
+      <p>{uid}</p>
     </>
   )
 }
