a6b962c421f6793eb33745d6ea246eed2e164f58
hisho <hisho.web@gmail.com>
2021-05-05 18:00:40 +0900

    📝 add commments to selector.ts

--- src/reducks/users/selector.ts
+++ src/reducks/users/selector.ts
@@ -3,11 +3,13 @@ import {RootStateType} from "@src/reducks/type";
 
 const usersSelector = (state: RootStateType) => state.users;
 
+//selectorを受け取り現在のユーザーidを返す関数
 export const getUserId = createSelector(
   [usersSelector],
   state => state.uid
 )
 
+//selectorを受け取り現在のユーザー名を返す関数
 export const getUsername = createSelector(
   [usersSelector],
   state => state.username
