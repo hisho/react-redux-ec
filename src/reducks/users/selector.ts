842b3777b09beb0933f5d1b14cd29ec2c96a922b
hisho <hisho.web@gmail.com>
2021-05-05 17:53:21 +0900

    ✨ add getUsername to selector.ts

--- src/reducks/users/selector.ts
+++ src/reducks/users/selector.ts
@@ -1,9 +1,14 @@
 import {createSelector} from "reselect";
 import {RootStateType} from "@src/reducks/type";
 
-const usersSelector = (state:RootStateType) => state.users;
+const usersSelector = (state: RootStateType) => state.users;
 
 export const getUserId = createSelector(
   [usersSelector],
   state => state.uid
+)
+
+export const getUsername = createSelector(
+  [usersSelector],
+  state => state.username
 )
