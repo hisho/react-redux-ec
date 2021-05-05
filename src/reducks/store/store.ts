c7214c9bd6af9b21f9bb80dc14f657975b68e35c
hisho <hisho.web@gmail.com>
2021-05-05 18:00:10 +0900

    📝 add commment to store.ts

--- src/reducks/store/store.ts
+++ src/reducks/store/store.ts
@@ -4,12 +4,15 @@ import {UsersReducer} from "@src/reducks/users/reducers";
 import {History} from "history";
 import thunk from "redux-thunk";
 
+//ストアを作成する
 export const createStore = (history: History) => {
   return reduxCreateStore(
+    //管理するステート
     combineReducers({
       router: connectRouter(history),
       users: UsersReducer
     }),
+    //ミドルウェアの設定
     applyMiddleware(
       routerMiddleware(history),
       thunk
