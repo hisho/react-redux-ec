435dbce8ba7b63c9a8131d088c0bd32bebfe23d3
hisho <hisho.web@gmail.com>
2021-05-07 03:56:27 +0900

    ✨ update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import {getUserId, getUsername} from "@src/reducks/users/selectors";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {signOut} from "@src/reducks/users/operations";
 
 export const Home: React.VFC = () => {
+  const dispatch = useDispatch();
   const selector = useSelector(state => state);
   const uid = getUserId(selector);
   const username = getUsername(selector);
@@ -12,6 +14,7 @@ export const Home: React.VFC = () => {
       this is home
       <p>ユーザーID：{uid}</p>
       <p>ユーザー名：{username}</p>
+      <button type="button" onClick={() => dispatch(signOut())}>サインアウト</button>
     </>
   )
 }
