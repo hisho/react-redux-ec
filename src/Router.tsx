518b8bca3cf911d5677dd08645903fdcbf8e1883
hisho <hisho.web@gmail.com>
2021-05-07 03:56:20 +0900

    ✨ import reset page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Route, Switch} from "react-router";
-import {Home, SignIn, SignUp} from "@src/pages";
+import {Home, SignIn, SignUp, Reset} from "@src/pages";
+import {Auth} from "@src/Auth";
 
 export const Router: React.VFC = () => {
   return (
     <Switch>
       <Route exact path="/signin" component={SignIn}/>
+      <Route exact path="/signin/reset" component={Reset}/>
       <Route exact path="/signup" component={SignUp}/>
       <Auth>
         <Route exact path="(/)?" component={Home}/>
