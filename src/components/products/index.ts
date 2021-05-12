7da399c59da82d7013c793437775e3359a4cd2b3
hisho <hisho.web@gmail.com>
2021-05-12 18:45:55 +0900

    ✨ add entry point to component products

---
+++ src/components/products/index.ts
@@ -0,0 +1,3 @@
+export {ImageArea} from "@src/components/products/ImageArea";
+export {ImagePreview} from "@src/components/products/ImagePreview";
+export {SetSizeArea} from "@src/components/products/SetSizeArea";
